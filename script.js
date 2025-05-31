f95d55f50b2c762432c0413cb3cc3ceee204dce4
Henrique Andrade Couto <hcouto76@gmail.com>
2025-05-31 12:14:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ project2.addEventListener("click", () => {
 //curriculo 
 const curriculo = document.getElementById('curriculo')
 curriculo.addEventListener("click", () => {
-    window.location.href = "curriculo/Curriculo - Henrique Andrade Couto.pdf"
+    window.location.href = "curriculo/Currículo - Henrique Andrade Couto.pdf"
 })
 
 //contatos 
